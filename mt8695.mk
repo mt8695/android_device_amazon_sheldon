e51c3514070715c8470ca576495ab1b8c73ce4d7
Bruno Martins <bgcngm@gmail.com>
2022-02-23 16:42:55 +0100

    mt8695-common: Build common basic USB HAL

--- mt8695.mk
+++ mt8695.mk
@@ -127,8 +127,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-impl \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.0-service.basic
 
 # Wi-Fi
 PRODUCT_COPY_FILES += \
