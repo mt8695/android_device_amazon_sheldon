f1bbec83b00d3048b0c711f16bae3aab4f3841f9
Zinadin Zidan <zidan44@pixelexperience.org>
2022-02-24 15:24:22 +0100

    mt8695-common: Patchelf MediaTek WiFi HAL
    * Load libwifi-hal-mtk.so instead of libwifi-hal.so [We cant build libwifi-hal from aosp as it will interrupt mtk wifi services so lets rename & pin]
    * Also load libwifi-hal-mtk.so in android.hardware.wifi@1.0-service-mediatek

--- extract-files.sh
+++ extract-files.sh
@@ -63,6 +63,9 @@ function blob_fixup() {
     	vendor/lib/libnvram.so)
             "${PATCHELF}" --add-needed "libshim_nvram.so" "${2}"
             ;;
+        vendor/bin/hw/android.hardware.wifi@1.0-service-mediatek)
+            "${PATCHELF}" --replace-needed "libwifi-hal.so" "libwifi-hal-mtk.so" "${2}"
+            ;;
     esac
 }
 
