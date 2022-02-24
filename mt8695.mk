cbd6cac9c56da65c8d5785f5996aad9209967a91
Roger Ortiz <rogerortizleal@gmail.com>
2022-02-24 10:48:57 +0100

    mt8695-common: add WifiOverlay

--- mt8695.mk
+++ mt8695.mk
@@ -29,6 +29,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(COMMON_PATH)/overlay
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# RRO
+PRODUCT_PACKAGES += \
+    WifiOverlay
+
 # Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
