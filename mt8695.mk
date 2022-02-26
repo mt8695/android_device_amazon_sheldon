190f9159e82ef3445e6c09b97a90553c16478bbf
Zinadin Zidan <zidan44@pixelexperience.org>
2022-02-26 13:10:35 +0100

    mt8695-common: Build libkeystore-engine-wifi-hidl and libkeystore-wifi-hidl

--- mt8695.mk
+++ mt8695.mk
@@ -151,6 +151,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-service \
     android.hardware.keymaster@3.0-impl
 
+PRODUCT_PACKAGES += \
+    libkeystore-engine-wifi-hidl \
+    libkeystore-wifi-hidl
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
