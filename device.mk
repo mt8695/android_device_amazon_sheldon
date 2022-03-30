3bef3ca9d766f9c646665ecced6df01e33be9f01
Debayan Kar <kardebayan3@gmail.com>
2022-03-30 15:57:04 +0200

    sheldon: Patchelf fireos.hardware.audio@4.0-impl.so for VNDK 30
    * Could not load /vendor/lib/hw/fireos.hardware.audio@4.0-impl.so
     from default namespace: dlopen failed: cannot locate symbol
     "_ZN7android8hardware5audio6common4V4_09HidlUtils16audioConfigToHalERKNS3_11AudioConfigEP12audio_config"
     referenced by "/vendor/lib/hw/fireos.hardware.audio@4.0-impl.so"

--- device.mk
+++ device.mk
@@ -220,7 +220,8 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v28/arm64/arch-arm-armv8-a/shared/vndk-core/libui.so:$(TARGET_COPY_OUT_VENDOR)/lib/libui-v28.so \
     prebuilts/vndk/v28/arm64/arch-arm-armv8-a/shared/vndk-core/libmedia_helper.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmedia_helper-v28.so \
     prebuilts/vndk/v28/arm64/arch-arm-armv8-a/shared/vndk-core/libxml2.so:$(TARGET_COPY_OUT_VENDOR)/lib/libxml2-v28.so \
-    prebuilts/vndk/v28/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v28.so
+    prebuilts/vndk/v28/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v28.so \
+    prebuilts/vndk/v28/arm64/arch-arm-armv8-a/shared/vndk-core/android.hardware.audio.common@4.0-util.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.hardware.audio.common@4.0-util_v28.so
 
 # Proprietary
 $(call inherit-product-if-exists, vendor/amazon/sheldon/sheldon-vendor.mk)
