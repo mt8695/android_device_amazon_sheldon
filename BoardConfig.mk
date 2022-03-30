4217987a58915baa47fede3aeec86293df7da4e1
R0rt1z2 <rogerortizleal@gmail.com>
2022-03-30 15:00:05 +0200

    sheldon: Import stock {manifest,compatibility_matrix}.xml
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,6 +93,10 @@ TARGET_USES_HWC2 := true
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
 
+# Vintf
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
+
 # Recovery
 TARGET_OTA_ASSERT_DEVICE := sheldon,sheldonp
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
